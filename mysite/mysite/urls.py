a2927c7d69f01ac7e896d47f9e0d1cf2a30cdff4
qwertygirl-coding <64401316+qwertygirl-coding@users.noreply.github.com>
2021-07-09 15:27:56 +0800

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from register import views as v
+from recipes import views as vr
 
 urlpatterns = [
-    path('/', include('recipes.urls')),
+    path('/', vr.index, name='home'),
     path('recipes/', include('recipes.urls')),
     path('admin/', admin.site.urls),
     path('register/', v.register, name='register'),
